c517750c0d35271e9a2dedb0ab0e195f92fc3959
Carol-Anne <81382405+CarolAnneC94@users.noreply.github.com>
2021-03-25 19:49:00 +0000

    User

---
+++ User.java
@@ -0,0 +1,26 @@
+package com.example.assignment1;
+
+public class User {
+    String UserName, Password;
+
+    public User(String userName, String password) {
+        UserName = userName;
+        Password = password;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+}
